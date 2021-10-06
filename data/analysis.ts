833f772774836e4b40c5e60e1153ed824792e34b
Abdulhakeem <abdulhakeemmustapha@gmail.com>
2021-10-06 22:56:16 +0100

    Implement function for returning rsi at tresholds

--- data/analysis.ts
+++ data/analysis.ts
@@ -1,22 +1,21 @@
-import { rejects } from "assert";
 import fs from "fs";
-import { resolve } from "path/posix";
-import { start } from "repl";
-import { Candle } from "./models";
+import { ApplyTo, Candle, IndicatorLevel, TalibFunctionReturn } from "./models";
 const talib = require("../node_modules/talib/build/Release/talib.node");
 
 export default class Analysis {
   private path: String;
   private marketData: Candle[];
 
-  constructor(path: string) {
+  constructor(path: string, depth = 1000) {
     this.path = path;
 
     if (!fs.existsSync(path)) {
       throw Error("File does not exist");
     }
     this.marketData = JSON.parse(fs.readFileSync(path, "utf-8"));
-    console.log(this.marketData.length);
+    this.marketData = this.marketData.splice(
+      Math.max(0, this.marketData.length - depth)
+    );
   }
 
   explain(func: string) {
@@ -31,7 +30,7 @@ export default class Analysis {
       talib.execute(
         {
           name: "ADX",
-          startIdx: Math.max(0, this.marketData.length - 2000),
+          startIdx: 0,
           endIdx: this.marketData.length - 1,
           high: this.marketData.map((candle) => candle.high),
           low: this.marketData.map((candle) => candle.low),
@@ -48,17 +47,20 @@ export default class Analysis {
     });
   }
 
-  rsi(period: number) {
+  rsi(
+    period: number = 14,
+    applyTo: ApplyTo = "high"
+  ): Promise<TalibFunctionReturn> {
     return new Promise((resolve, reject) => {
       talib.execute(
         {
           name: "RSI",
-          startIdx: Math.max(0, this.marketData.length - 2000),
+          startIdx: 0,
           endIdx: this.marketData.length - 1,
-          inReal: this.marketData.map((candle) => candle.high),
+          inReal: this.marketData.map((candle) => candle[applyTo]),
           optInTimePeriod: period,
         },
-        function (err: Object, result: Object) {
+        function (err: Object, result: TalibFunctionReturn) {
           if (err) {
             return reject(err);
           }
@@ -67,10 +69,46 @@ export default class Analysis {
       );
     });
   }
+
+  rsiBuyLevels(
+    period: number = 14,
+    applyTo: ApplyTo = "high",
+    threshold = 23.5
+  ) {
+    return this.rsi().then((rsi) => {
+      const rsiLevels = rsi.result.outReal;
+      rsiLevels.splice(0, 0, ...Array(rsi.begIndex));
+      const marketData: IndicatorLevel[] = this.marketData
+        .map((candle, index) => {
+          return { timestamp: candle.timestamp, level: rsiLevels[index] };
+        })
+        .filter((level) => level.level <= threshold);
+      return marketData;
+    });
+  }
+
+  rsiSellLevels(
+    period: number = 14,
+    applyTo: ApplyTo = "high",
+    threshold = 75
+  ) {
+    return this.rsi().then((rsi) => {
+      const rsiLevels = rsi.result.outReal;
+      rsiLevels.splice(0, 0, ...Array(rsi.begIndex));
+      const marketData: IndicatorLevel[] = this.marketData
+        .map((candle, index) => {
+          return { timestamp: candle.timestamp, level: rsiLevels[index] };
+        })
+        .filter((level) => level.level >= threshold);
+      return marketData;
+    });
+  }
 }
 
 // new Analysis("./ohlc/ALGO15.json").adx(9).then(console.log);
 // new Analysis("./ohlc/ALGO15.json").rsi(9).then(console.log);
+// new Analysis("./ohlc/ALGO15.json").rsiBuyLevels(25).then(console.log);
+new Analysis("./ohlc/ALGO15.json").rsiSellLevels().then(console.log);
 
 // new Analysis("./ohlc/ALGO15.json").explain("CDL3BLACKCROWS").then((details) => {
 //   console.dir(details, { depth: null });
--- data/models.ts
+++ data/models.ts
@@ -1,24 +1,38 @@
-
+import { Candle } from "./models";
 export enum IntervalDuration {
-  '5m' = 300,
-  '15m' = 900,
-  '30m' = 1800,
-  '1h' = 3600,
-  '4h' = 3600 * 4,
-  '1d' = 86400,
+  "5m" = 300,
+  "15m" = 900,
+  "30m" = 1800,
+  "1h" = 3600,
+  "4h" = 3600 * 4,
+  "1d" = 86400,
 }
 
-export type Interval = keyof typeof IntervalDuration
-
+export type Interval = keyof typeof IntervalDuration;
 
 export interface Candle {
-  open: number,
-  high: number,
-  low: number,
-  close: number,
-  timestamp: number
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  timestamp: number;
 }
 
 export interface MetaCandle extends Candle {
-  volume: number
+  volume: number;
 }
+
+export type ApplyTo = "open" | "high" | "low" | "close";
+
+export type TalibFunctionReturn = {
+  begIndex: 17;
+  nbElement: 983;
+  result: {
+    outReal: number[];
+  };
+};
+
+export type IndicatorLevel = {
+  timestamp: number;
+  level: number;
+};
